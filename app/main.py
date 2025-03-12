523a2d3f3e7d2ded73a036dd12a3027341d8d964
Mathes Freitas <sql1freitas@gmail.com>
2025-03-12 11:08:46 -0300

    Realizada verificação de erro em divisão por 0.
    Também filtrado casos de listas vazias

--- app/main.py
+++ app/main.py
@@ -10,17 +10,21 @@ class CarWashStation:
         self, distance_from_city_center: float, clean_power: int,
         average_rating: float, count_of_ratings: int
     ):
-        self.distance_from_city_center = distance_from_city_center
+        self.distance_from_city_center = max(distance_from_city_center, 0.1)
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
+
     def calculate_washing_price(self, car: Car) -> float:
+        if self.distance_from_city_center == 0:
+            raise ValueError("distance_from_city_center cannot be zero.")
         price = (
             car.comfort_class * (self.clean_power - car.clean_mark) *
             self.average_rating / self.distance_from_city_center
         )
         return round(price, 1)
+
 
     def wash_single_car(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
@@ -29,11 +33,15 @@ class CarWashStation:
             return price
         return 0.0
 
+
     def serve_cars(self, cars: list) -> float:
+        if not cars:
+            return 0.0
         total_income = 0.0
         for car in cars:
             total_income += self.wash_single_car(car)
         return round(total_income, 1)
+
 
     def rate_service(self, rating: int):
         self.average_rating = round(
